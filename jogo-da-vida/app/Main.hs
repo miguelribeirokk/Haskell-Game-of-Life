992702bf4ebaa2165d070151ba5277696980e029
aluno caf <miguel.a.silva@ufv.br>
2023-09-29 14:06:47 -0300

    atualizando main

--- jogo-da-vida/app/Main.hs
+++ jogo-da-vida/app/Main.hs
@@ -1,37 +1,37 @@
 module Main where
 
 import           Data.Matrix (Matrix, fromLists, prettyMatrix)
-import qualified Data.Matrix as M
+import qualified Data.Matrix as M 
 import           Data.Maybe (fromMaybe)
 
 lerInicial :: String -> IO (Matrix Bool)
 lerInicial fp = do
-        texto <- readFile fp 
-        let linhas = lines texto 
-        let booleanos = map (map(=='#')) linhas
-        return (fromLists booleanos)
+    texto <- readFile fp 
+    let linhas = lines texto
+    let booleanos = map (map (== '#')) linhas
+    return(fromLists booleanos)
 
-ataulizar :: MatrixBool -> Matrix Bool
-atualizar m = M.mapPos () m
+atualizar :: Matrix Bool -> Matrix Bool
+atualizar m = M.mapPos (atualizarCelula m) m
 
-atualizarCelula :: MatrixBool -> (Int, Int) -> Bool -> Bool
-atualizarCelula m (i,j) v =(v && (vivos ==2 || vivos ==3)) || vivos == 3
+atualizarCelula :: Matrix Bool -> (Int, Int) -> Bool -> Bool
+atualizarCelula m (i, j) v = (v && (vivos == 2 || vivos == 3)) || vivos == 3
+    where
+        deslocamentos = [(-1, -1), (-1, 0), (-1, 1),
+                         (0, -1),           (0, 1),
+                         (1, 1),   (1, 0),  (1, 1)]
+        vizinhos = map (\(di,dj) -> M.safeGet (i+di) (j+dj) m) deslocamentos
+        vizinhos' = map (fromMaybe False) vizinhos
+        vivos = length (filter (\x -> x) vizinhos')
 
-atualizarCelula m(i,j) v=v
-        where
-                deslocamentos =[(-1,-1), (-1,0), (-1,1),
-                                (0,-1)           (0, 1),
-                                (1,-1), (1,0),   (1, 1)]
-                vizinhos = map (\(di,dj)-> M.safeGet (i+di) (j+dj) m) deslocamentos 
-                vizinhos' = map (fromMaybe False) vizinhos
-                vivos = lenght (filter (\x -> x) vizinhos')
- main :: IO() 
- main = do  
-    let fp = "../teste.txt"
+
+main :: IO ()
+main = do
+    let fp = "teste.txt"
     m <- lerInicial fp
-    putStrLn ""
+    run m
     where 
         run m = do
-                putStrl (prettyMatrix m)
-                run (atualizar m)
-
+            putStrLn (prettyMatrix m)        
+            run (atualizar m)
+mostrarMatriz :: Matrix
