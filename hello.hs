0435ae4d066626b9410183b64993cb4056149b73
Miguel R <miguel.a.silva@ufv.br>
2023-09-29 08:42:49 -0300

    ib1 e fib 2

--- hello.hs
+++ hello.hs
@@ -12,8 +12,13 @@ bhaskara a b c =
         else if delta == 0 then [x'] else [x', x'']
 -}
 
+
+
+delta :: Floating a => a -> a -> a -> a
 delta a b c = b**2 - 4*a*c
 
+
+bhaskara :: (Ord a, Floating a) => a -> a -> a -> [a]
 bhaskara a b c
     | d < 0 = []
     | d == 0 = [x']
@@ -22,3 +27,19 @@ bhaskara a b c
         d = delta a b c
         x' = (-b + sqrt d) / (2*a)
         x'' = (-b - sqrt d) / (2*a)
+
+fib  :: Int ->  Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib(n-2)
+
+fib2 :: Int -> Int
+fib2 0 = 0
+fib2 1 = 1
+fib2 n = fib' 0 1 2
+    where
+        fib' n2 n1 i =
+            if i == n
+                then n2 + n1 
+                else fib' n1 (n2+n1) (i+1)
+    
