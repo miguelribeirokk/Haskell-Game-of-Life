fe94d8dc342628d329c1203451beef80edb5a00c
Miguel R <miguel.a.silva@ufv.br>
2023-09-29 11:12:41 -0300

    lookupBy

--- maybe.hs
+++ maybe.hs
@@ -11,3 +11,15 @@ indiceLetra c = fromMaybe 0 m
         Just i -> i
     -}
     where m = lookup c (zip ['A'..'Z'][1..])
+
+indiceLetra2 :: Char -> Maybe Int
+indiceLetra2 c = lookup c (zip ['A'..'Z'][1..])
+
+-- lookupBy  --> Busca um elemento em uma lista com base em uma função de teste
+-- lookupBy (\p -> head p == 'a') ["livro", "asas", "casa"]
+lookupBy :: (a -> Bool) -> [a] -> Maybe a
+lookupBy f [] = Nothing
+lookupBy f (h:t) =
+    if f h
+        then Just h
+        else lookupBy f t
