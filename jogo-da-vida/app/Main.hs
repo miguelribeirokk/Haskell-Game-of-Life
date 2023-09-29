bc7dd0783ca485b7a0c89c105debc448ad226260
aluno caf <miguel.a.silva@ufv.br>
2023-09-29 13:40:48 -0300

    atualizarCelula

--- jogo-da-vida/app/Main.hs
+++ jogo-da-vida/app/Main.hs
@@ -1,4 +1,31 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import           Data.Matrix (matrix, fromLists)
+import qualified Data.Matrix as M
+import           Data.Maybe (fromMaybe)
+
+lerInicial :: String -> IO (Matrix Bool)
+lerInicial fp = do
+        texto <- readFile fp 
+        let linhas = lines texto 
+        let booleanos = map (map(=='#')) linhas
+        return (fromLists booleanos)
+
+ataulizar :: MatrixBool -> Matrix Bool
+atualizar m = M.mapPos () m
+
+atualizarCelula :: MatrixBool -> (Int, Int) -> Bool -> Bool
+atualizarCelula m(i,j) v=v
+        where
+                deslocamentos =[(-1,-1), (-1,0), (-1,1),
+                                (0,-1)           (0, 1),
+                                (1,-1), (1,0),   (1, 1)]
+                vizinhos = map (\(di,dj)-> M.safeGet (i+di) (j+dj) m) deslocamentos 
+                vizinhos' = map (fromMaybe False) vizinhos
+                vivos = lenght (filter (\x -> x) vizinhos')
+ main :: IO() 
+ main = do  
+    let m = fromLists [[1,2], [3,4]]
+    putStrLn (prettymatrix m)
+
+       
